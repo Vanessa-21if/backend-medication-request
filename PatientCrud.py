7635804aa4017c09ab99fcfcc2aefcc5e9bad92f
Vanessa-21if <21vanessaaa@gmail.com>
2025-04-04 18:23:28 -0500

    Create PatientCrud.py

---
+++ PatientCrud.py
@@ -0,0 +1,35 @@
+from connection import connect_to_mongodb
+from bson import ObjectId
+from fhir.resources.patient import Patient
+import json
+collection = connect_to_mongodb("EntregaDeMedicamentos", "pacientes")
+def GetPatientById(patient_id: str):
+    try:
+        patient = collection.find_one({"_id": ObjectId(patient_id)})
+        if patient:
+            patient["_id"] = str(patient["_id"])
+            return "success", patient
+        return "notFound", None
+    except Exception as e:
+        return f"notFound", None
+def WritePatient(patient_dict: dict):
+    try:
+        pat = Patient.model_validate(patient_dict)
+    except Exception as e:
+        return f"errorValidating: {str(e)}",None
+    validated_patient_json = pat.model_dump()
+    result = collection.insert_one(patient_dict)
+    if result:
+        inserted_id = str(result.inserted_id)
+        return "success",inserted_id
+    else:
+        return "errorInserting", None
+def GetPatientByIdentifier(patientSystem,patientValue):
+    try:
+        patient = collection.find_one({"identifier.system":patientSystem,"identifier.value":patientValue})
+        if patient:
+            patient["_id"] = str(patient["_id"])
+            return "success", patient
+        return "notFound", None
+    except Exception as e:
+        return f"error: {str(e)}", None
